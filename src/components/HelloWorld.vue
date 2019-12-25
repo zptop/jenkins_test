52671b5bdbbfdeb0d7372d0537028c2854470696
zptop <249909846@qq.com>
2019-12-25 12:09:13 +0800

    1225

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1>今天是2019/11/26日，项目终于执行成功了~</h1>
+    <h1>今天是2019/12/25日，项目执行成功了~</h1>
     <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
